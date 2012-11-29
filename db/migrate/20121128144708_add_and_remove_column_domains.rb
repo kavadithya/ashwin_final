9ee39c6ea942ed61f07f977ec977c7d7706a9736
Adithya <kav.adithya@gmail.com>
2012-11-29 07:34:07 +0530

    0

--- db/migrate/20121128144708_add_and_remove_column_domains.rb
+++ db/migrate/20121128144708_add_and_remove_column_domains.rb
@@ -1,7 +1,7 @@
 class AddAndRemoveColumnDomains < ActiveRecord::Migration
   def change
   	#remove_column :users, :ownerid, :integer
-  	remove_column :domains, :owner, :string
+  	#remove_column :domains, :owner, :string
   	add_column :domains, :ownerid, :integer
   end
 end
