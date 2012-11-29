702a848079c79ba095b7cc10fbc573cb61839ff6
Adithya <kav.adithya@gmail.com>
2012-11-29 07:25:19 +0530

    4

--- db/migrate/20121128143956_add_and_remove_column_users.rb
+++ db/migrate/20121128143956_add_and_remove_column_users.rb
@@ -1,6 +1,6 @@
 class AddAndRemoveColumnUsers < ActiveRecord::Migration
   def change
   	add_column :users, :ownerid, :integer
-  	remove_column :users, :owner, :string
+  	#remove_column :users, :owner, :string
   end
 end
